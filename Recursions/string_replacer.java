819122c5bd091c62ae684f36f3980ee5aefcd1ed
João <40552750+Joao-Regis@users.noreply.github.com>
2020-03-14 22:08:39 -0700

    Create string_replacer.java

---
+++ Recursions/string_replacer.java
@@ -0,0 +1,84 @@
+
+
+public class string_replacer 
+{
+  public static void main(String[] args) 
+  {
+    String rplcMssg;
+    String word = "Hello";
+    int start = 0;
+    int charRplcd = 0;
+    strRplc(word, start, charRplcd);
+    //System.out.println(strRplc(word));        
+  }
+
+  public static void strRplc(String str, int index, int lettersReplaced)
+  {   
+    int loc = index;
+    int rplcd = lettersReplaced;
+    String strMod = str;
+      
+    if (loc == (strMod.length()))  // Reached the end of the string
+    {
+      System.out.println("Result from recursive replacement: " + strMod + ". ");
+      System.out.println("Number of characters replaced " + rplcd);
+    }
+    else if (loc < strMod.length())
+    {
+      if(strMod.charAt(loc) == 'a')
+      {
+        strMod = strMod.replace('a', 'e');
+        rplcd++;
+      }
+      else if(strMod.charAt(loc) == 'e')
+      {
+        strMod = strMod.replace('e', 'i');
+        rplcd++;
+      }
+      else if(strMod.charAt(loc) == 'i')
+      {
+        strMod = strMod.replace('i', 'o');
+        rplcd++;
+      }
+      else if(strMod.charAt(loc) == 'o')
+      {
+        strMod = strMod.replace('o', 'u');
+        rplcd++;
+      }
+      else if(strMod.charAt(loc) == 'u')
+      {
+        strMod = strMod.replace('u', 'a');
+        rplcd++;
+      }
+      
+      loc++;
+      strRplc(strMod, loc, rplcd);
+    }
+  }
+
+}
+
+
+/*
+3. String replacer
+Write a recursive method that accepts a 
+string as its argument and replaces 
+the following characters.
+
+a with e
+e with i
+i with o
+o with u
+u with a
+
+The output should display the replaced      
+string and total number of replacements. 
+
+For example:
+
+Hello would be Hillu 
+Number of characters replaced 2
+
+for loop run multiple times implementing one replace at a time.
+for loop move letter by letter implementing all changes.
+*/
