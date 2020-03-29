9efeaae48b80509c8eb2743d49fe39dc983883a3
João <40552750+Joao-Regis@users.noreply.github.com>
2020-03-29 11:12:10 -0700

    Create highest_and_lowest_demo.java.java

---
+++ Generics/highest_and_lowest_elements/highest_and_lowest_demo.java.java
@@ -0,0 +1,20 @@
+public class highest_and_lowest_demo
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+
+   
+}
+
+
+
+/*
+6. Highest and Lowest Elements
+Write a generic class with a type parameter constrained to 
+any type that implements Comparable. The constructor should accept 
+an array of such objects. The class should have methods that return 
+the highest and lowest values in the array. Demonstrate the class 
+in an application.
+*/
